85ce4eb0e43e4ef594cf53748ef1cc5ffec3e6e3
kalyanisborse2005-sudo <kalyanisborse2005@gmail.com>
2026-08-19 20:19:58 +0530

    Add files via upload
    This lab covers SQL table creation and modification in a Banking Database, including:
    
    Creating tables
    Adding/modifying columns
    Adding Primary Key, Foreign Key, UNIQUE, NOT NULL, and CHECK constraints
    Describing tables
    Dropping a table
    
    In short: It focuses on DDL commands and table constraints.

---
+++ sql lab 1.sql
@@ -0,0 +1,59 @@
+create database BankingDB;
+use BankingDB;
+CREATE TABLE Customers
+(
+    CustomerID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15)
+);
+
+describe Customers;
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+DROP TABLE AccountBranches;
+ALTER TABLE Customers
+ADD PRIMARY KEY (CustomerID);
+ALTER TABLE Accounts
+ADD CustomerID INT;
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+ALTER TABLE Customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+ALTER TABLE Customers
+ADD CONSTRAINT uq_Email UNIQUE (Email);
