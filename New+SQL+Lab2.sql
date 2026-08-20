1638d6b8cd7b5bc543bc42cd5cd6d4b34d516a0d
kalyanisborse2005-sudo <kalyanisborse2005@gmail.com>
2026-08-20 07:50:22 +0530

    Add files via upload
    This project contains a comprehensive collection of SQL queries for designing and managing a relational banking database using MySQL. It covers essential SQL and database concepts, including database and table creation, primary and foreign keys, constraints, table relationships, data insertion, updating and deletion, and retrieving information using SQL queries.
    
    The project demonstrates practical implementation of SQL concepts through a structured banking database, helping build a strong understanding of relational database management and data manipulation.
    
    Key Concepts: Database Design, DDL, DML, Constraints, Primary Keys, Foreign Keys, Table Relationships, Data Manipulation, and SQL Queries.

---
+++ New+SQL+Lab2.sql
@@ -0,0 +1,153 @@
+create database BankingDB;
+use BankingDB;
+CREATE TABLE Customers
+(
+    CustomerID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15)
+);
+
+describe Customers;
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches ( 
+		AssignmentDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE
+);
+
+Describe Accounts;
+Describe Transactions;
+describe Branches;
+describe AccountBranches;
+describe Loans;
+
+ALTER TABLE Customers
+ADD DateOfBirth DATE;
+
+describe Customers;
+
+ALTER TABLE Customers
+MODIFY Phone VARCHAR(20);
+
+ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
+DROP TABLE AccountBranches;
+
+ALTER TABLE Customers
+ADD PRIMARY KEY (CustomerID);
+
+ALTER TABLE Accounts
+ADD CustomerID INT;
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Customers
+MODIFY FirstName VARCHAR(50) NOT NULL;
+
+ALTER TABLE Customers
+ADD CONSTRAINT uq_Email UNIQUE (Email);
+
+
+#########################################
+-- Add Primary Keys
+ALTER TABLE Accounts
+ADD CONSTRAINT PK_Accounts
+PRIMARY KEY (AccountID);
+
+ALTER TABLE Transactions
+ADD CONSTRAINT PK_Transactions
+PRIMARY KEY (TransactionID);
+
+ALTER TABLE Branches
+ADD CONSTRAINT PK_Branches
+PRIMARY KEY (BranchID);
+
+ALTER TABLE Loans
+ADD CONSTRAINT PK_Loans
+PRIMARY KEY (LoanID);
+
+-- Add Required Columns
+
+
+ALTER TABLE Transactions
+ADD AccountID INT;
+
+ALTER TABLE Loans
+ADD CustomerID INT;
+
+
+ALTER TABLE Transactions
+ADD CONSTRAINT FK_Transactions_Accounts
+FOREIGN KEY (AccountID)
+REFERENCES Accounts(AccountID);
+
+ALTER TABLE Loans
+ADD CONSTRAINT FK_Loans_Customers
+FOREIGN KEY (CustomerID)
+REFERENCES Customers(CustomerID);
+
+ALTER TABLE Accounts
+ADD BranchID INT;
+
+ALTER TABLE Accounts
+ADD CONSTRAINT FK_Accounts_Branches
+FOREIGN KEY (BranchID)
+REFERENCES Branches(BranchID);
+
+INSERT INTO Customers
+(CustomerID, FirstName, LastName, Email, Phone, DateOfBirth)
+VALUES
+(101,'Rahul','Sharma','rahul@gmail.com','9876543210','1998-04-15');
+INSERT INTO Accounts
+(AccountID, CustomerID, AccountType, Balance)
+VALUES
+(201,101,'Savings',25000);
+SET SQL_SAFE_UPDATES=0 ;
+UPDATE Customers
+SET Phone='9999999999'
+WHERE CustomerID=101;
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+UPDATE Customers
+SET Email='rahul.sharma@gmail.com'
+WHERE CustomerID=101;
+SELECT * FROM Customers
+WHERE CustomerID = 101;
+DELETE FROM Transactions
+WHERE TransactionID = 302;
+SELECT * FROM Transactions;
+DELETE FROM Accounts
+WHERE AccountID = 202;
+SELECT * FROM Accounts;
